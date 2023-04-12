663c94d063887c04f29ccb82d92683dbd3ba5732
shkartmaz <sashakh@live.ru>
2023-04-12 17:27:02 +0300

    introduced phases comosition
    not operational yet
    needs further work

--- CA_cryst.py
+++ CA_cryst.py
@@ -2,23 +2,36 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import colors
+from dataclasses import dataclass
 
 
-# попытка смоделировать магматическую камеру как клеточный автомат
-# v0.2
-# кристаллы оливина случайным образом формируются в однородной толще расплава
-# и тонут. Температура системы постоянна
+#   попытка смоделировать магматическую камеру как клеточный автомат
+#   v0.3
+#   трех (пяти, семи итд) значений температуры явно мало
+#   поэтому придется добавлять температуру как отдельное свойство
+#   а может быть, и состав
 
-neighbourhood = ((-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,1), (1,0), (1,1))
-L2, L1, L0 = 0, 1, 2
-OL, ROOF = 3, 4
-BACKGROUND = 5
-cmap2 = colors.ListedColormap(['yellow', 'gold', 'orange', 'green', 'dimgray', 'black'])
+## Template: {type, temperature, MgO, FeO2, SiO2, CaO, Al2O3}
 
+LIQUID = 0
+ROOF, BACKGROUND = 1, 2
+OL, PL, OPX, CPX = 3, 4, 5, 6
 TIME_INTERVAL = 50
 DEBUG_MODE = False
-##bounds = [0,1,2,3,4]
-##norm = colors.BoundaryNorm(bounds, cmap2.N)
+
+@dataclass
+class cell:
+    phase:  int = LIQUID
+    temperature:    float = 1500
+    MgO:    float = 10
+    FeO:    float = 10
+    CaO:    float = 10
+    SiO2:   float = 10
+    Al2O3:  float = 10
+
+cmap2 = colors.ListedColormap(['yellow', 'gold', 'orange', 'green', 'dimgray', 'black'])
+
+
 
 def debug(output):
     if DEBUG_MODE:
@@ -26,28 +39,28 @@ def debug(output):
 
 def iterate(X):
     """Итерации магматической камеры"""
-
-    X1 = np.zeros((ny, nx))
-    for ix in range(nx):
-        for iy in range(ny):
-            if X[iy,ix] in (ROOF, BACKGROUND):
-                X1[iy,ix] = X[iy,ix]
-
-            elif X[iy,ix] == OL and X[iy+1,ix] not in (OL,ROOF):
-                #debug('[OL]\tX \t-->\t [X]')
-                X1[iy,ix] == X[iy+1,ix]
-            elif X[iy,ix] not in [OL,ROOF] and X[iy-1,ix] == OL:
-                #debug('OL\t[X] \t-->\t [OL]')
-                X1[iy,ix] = OL
-           
-            elif X[iy,ix] == OL:
-                #debug('[OL] \t-->\t [OL]')
-                X1[iy,ix] = OL
-            
-            elif X[iy,ix] == L2 and np.random.random() < 0.005:
-                X1[iy,ix] = OL
-             
-    return X1
+    pass
+##    X1 = np.zeros((ny, nx))
+##    for ix in range(nx):
+##        for iy in range(ny):
+##            if X[iy,ix] in (ROOF, BACKGROUND):
+##                X1[iy,ix] = X[iy,ix]
+##
+##            elif X[iy,ix] == OL and X[iy+1,ix] not in (OL,ROOF):
+##                #debug('[OL]\tX \t-->\t [X]')
+##                X1[iy,ix] == X[iy+1,ix]
+##            elif X[iy,ix] not in [OL,ROOF] and X[iy-1,ix] == OL:
+##                #debug('OL\t[X] \t-->\t [OL]')
+##                X1[iy,ix] = OL
+##           
+##            elif X[iy,ix] == OL:
+##                #debug('[OL] \t-->\t [OL]')
+##                X1[iy,ix] = OL
+##            
+##            elif X[iy,ix] == L2 and np.random.random() < 0.005:
+##                X1[iy,ix] = OL
+##             
+##    return X1
 
 
 fig = plt.figure(facecolor='black',figsize=(5,5))
@@ -55,11 +68,11 @@ ax = fig.add_subplot(111)
 ax.set_axis_off()
 
 nx, ny = 30, 30
-X = np.zeros((ny, nx))
-X[0] = [ROOF]*nx
-X[ny-1] = [BACKGROUND]*nx
-X[ny-2] = [ROOF]*nx
-X[20,5] = OL
+X = np.array([[cell()]*nx]*ny)
+X[0] = [cell(ROOF)]*nx
+X[ny-1] = [cell(BACKGROUND)]*nx
+X[ny-2] = [cell(ROOF)]*nx
+X[20,5] = cell(OL)
 
 im = ax.imshow(X, cmap = cmap2, interpolation = 'nearest')
 
