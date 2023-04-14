a1ec7b8d1f8e3fb1fe8df6633023050898f2bea1
shkartmaz <sashakh@live.ru>
2023-04-14 18:46:33 +0300

    More beauty
    Removed some obsolete stuff
    Wrote comments

--- CA_cryst.py
+++ CA_cryst.py
@@ -1,18 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
 import pygame
 
-
-# попытка смоделировать магматическую камеру как клеточный автомат
-# визуализация на pygame
-# работает!
-
-neighbourhood = ((-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,1), (1,0), (1,1))
-L2, L1, L0 = 0, 1, 2
+# cell phase color constants
+L = 0
 OL, ROOF = 3, 4
 BACKGROUND = 5
-
 COLORS = [
     (255,255,50),
     (0,0,200),
@@ -21,83 +13,73 @@ COLORS = [
     (50,50,50),
     (0,0,0)
     ]
+CRYST_PROBABILITY = 0.005
 
+# visuals setup
 SCREEN_WIDTH = 250
 SCREEN_HEIGHT = 250
 CELL_SIZE = 10
 BOTTOM_OFFSET = 0
-    
-TIME_INTERVAL = 50
-DEBUG_MODE = False
-##bounds = [0,1,2,3,4]
-##norm = colors.BoundaryNorm(bounds, cmap2.N)
-
-def debug(output):
-    if DEBUG_MODE:
-        print(output)
 
 def iterate(X):
-    """Итерации магматической камеры"""
+    """Perform a step from X state according to the rules and return new state X1"""
 
     X1 = np.zeros((ny, nx))
     for ix in range(nx):
         for iy in range(ny):
+            # these cells stay the same
             if X[iy,ix] in (ROOF, BACKGROUND):
                 X1[iy,ix] = X[iy,ix]
 
+            # OL sinks down one cell
+            # Done in two steps
             elif X[iy,ix] == OL and X[iy+1,ix] not in (OL,ROOF):
-                #debug('[OL]\tX \t-->\t [X]')
-                X1[iy,ix] == X[iy+1,ix]
+                X1[iy,ix] == X[iy+1,ix]   
             elif X[iy,ix] not in [OL,ROOF] and X[iy-1,ix] == OL:
-                #debug('OL\t[X] \t-->\t [OL]')
-                X1[iy,ix] = OL
-           
-            elif X[iy,ix] == OL:
-                #debug('[OL] \t-->\t [OL]')
                 X1[iy,ix] = OL
             
-            elif X[iy,ix] == L2 and np.random.random() < 0.005:
+            # If OL has nowhere to sink, it stays in place
+            elif X[iy,ix] == OL:
                 X1[iy,ix] = OL
-             
+            
+            # every Liquid cell crystallizes with a set chance
+            elif X[iy,ix] == L and np.random.random() < CRYST_PROBABILITY:
+                X1[iy,ix] = OL         
     return X1
 
 def draw(X):
+    """Draw a rectangle of correcponding for each cell"""
     for ix in range(nx):
         for iy in range(ny):
             pygame.draw.rect(screen, COLORS[int(X[iy,ix])], [ix*CELL_SIZE,
                                                              iy*CELL_SIZE,
                                                              (ix+1)*CELL_SIZE,
                                                              (iy+1)*CELL_SIZE])
-            
     pygame.display.update()
     
-
-
-pygame.init()
-
-
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
-
+# initialize the chamber data structure
 nx, ny = SCREEN_WIDTH // CELL_SIZE, SCREEN_HEIGHT // CELL_SIZE - BOTTOM_OFFSET
 X = np.zeros((ny, nx))
 X[0] = [ROOF]*nx
 X[ny-1] = [BACKGROUND]*nx
 X[ny-2] = [ROOF]*nx
-#X[20,5] = OL
 
-draw(X)
+# initialize pygame
+pygame.init()
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-iteration = 0
+# main cycle
+iteration = 0  # only used for screenshots as of now
 running = True
 while running:
+    # making screenshots
     #name = "s" + str(iteration) +".jpg"
     #pygame.image.save(screen, name)
+    draw(X)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    draw(X)
     X = iterate(X)
-    iteration+=1
+    iteration += 1
 
 pygame.quit()
